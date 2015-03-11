66f8f5364ce3ed8dcbf069f553a9dceb76345534
Rick Hall <rickhall2000@hotmail.com>
2015-03-10 22:40:50 -0400

    async demo

--- demo/src/demo/async.clj
+++ demo/src/demo/async.clj
@@ -2,43 +2,55 @@
   (:require [clojure.core.async :refer :all])
   (:import [java.lang.Thread]))
 
-(def input-chan (chan))
-(def output-chan (chan))
-(def input-chan' (chan))
-(def output-chan' (chan))
+(def dbl-in (chan))
+(def dbl-out (chan))
+(def sq-in (chan))
+(def sq-out (chan))
 
-(defonce slow-doubler
+(def slow-doubler
   (go-loop []
-   (let [input (<! input-chan)]
+   (let [input (<! dbl-in)]
      (Thread/sleep 2000)
-     (>! output-chan (* 2 input)))
+     (>! dbl-out (* 2 input)))
    (recur)))
 
-(defonce slow-square
+(def slow-square
   (go-loop []
            (let [delay (+ 1500 (rand-int 1000))
-                 input (<! input-chan')]
+                 input (<! sq-in)]
              (Thread/sleep delay)
-             (>! output-chan' (* input input)))
+             (>! sq-out (* input input)))
            (recur)))
 
 (defn blocking
   [n]
-  (>!! input-chan n)
+  (>!! dbl-in n)
   (println "Now we wait")
-  (println "I got back" (<!! output-chan)))
+  (println "I got back" (<!! dbl-out)))
 
 (defn non-blocking
   [n]
   (go
-   (>! input-chan n)
+   (>! dbl-in n)
    (println "Waiting for result")
-   (println "I got back" (<! output-chan)))
+   (println "I got back" (<! dbl-out)))
   (println "I can keep doing things while I wait"))
 
 (defn blocking-square
   [n]
   (time
    (do
-     (>!! input-chan' n)
-     (<!! output-chan'))))
+     (>!! sq-in n)
+     (<!! sq-out))))
+
+(defn some-test
+  [n]
+  (go
+    (>! dbl-in n)
+    (>! sq-in n)
+    (let [[v c] (alts! [dbl-out sq-out])]
+      (println "I got value " v "from channel: " c))))
+
+(defn clear-chans
+  []
+  (alts!! [(timeout 3000) dbl-out sq-out dbl-in sq-in]))
